ae4b2cee00e58bca235b71a8dc07839309964b26
Githubuser289 <moldovan.mms@gmail.com>
2024-01-21 19:21:24 +0200

    small adjustment

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -11,7 +11,7 @@ class ContactForm extends Component {
           <input
             type="text"
             name="name"
-            // pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
+            pattern="^[a-zA-Z]+((['\-][a-zA-Z])?[a-zA-Z]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -22,7 +22,7 @@ class ContactForm extends Component {
           <input
             type="tel"
             name="number"
-            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
@@ -37,4 +37,5 @@ class ContactForm extends Component {
 ContactForm.propTypes = {
   submitCallback: PropTypes.func.isRequired,
 };
+
 export default ContactForm;
